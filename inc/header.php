c5320e54011bbd493922fe7b8f610f03a73530eb
Tegon-McClouds <Tegon.mccloud@gmail.com>
2020-03-28 13:40:40 +0100

    create header with navigation bar¨

---
+++ inc/header.php
@@ -0,0 +1,7 @@
+
+<nav class="nav-bar">
+	<h1>File Storage</h1>
+	<a href="signup.php">Sign up</a>
+	<a href="login.php">Log in</a>
+	<a href="files.php">Files</a>
+</nav>
---
+++ index.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Over+the+Rainbow&display=swap" rel="stylesheet">
+	<link href="css/style.css" type="text/css" rel="stylesheet">
+	<title>File storage</title>
+</head>
+<body>
+	<?php
+	include 'inc/header.php';
+	?>
+
+	 <?php
+	 include 'inc/footer.php';
+	 ?>
+
+</body>
+</html>
