2e8d11fb873a905ddd53eea617a8b5f8a26d0756
denbedstegamer <54136946+denbedstegamer@users.noreply.github.com>
2020-05-01 17:48:20 +0200

    fixed deleting accounts and added features to delete files that the user had

--- public_html/php/delete_account.php
+++ public_html/php/delete_account.php
@@ -9,14 +9,33 @@
 	}
 	
 	$DB = openDB();
-	$result = mysqli_query($DB, "CALL delete_account(".$_SESSION["accountid"].")");
+	$result = mysqli_query($DB, "CALL get_file_paths(".$_SESSION["accountid"].", 1000)");
 	if(!$result) {
-		echo("Failed to delete the account from database:<br>");
+		echo("Failed to delete the files from database:<br>");
 		echo mysqli_error($DB);
 	}
+
+	while($row = mysqli_fetch_array($result)) {
+		$path = "../uploads/".$_SESSION["accountid"]."/";
+		$fpath = realpath($path.$row[0]);
+		if (!unlink($fpath)) {
+			echo ("$fpath cannot be deleted due to an error");
+		}
+		else {
+			echo ("$fpath has been deleted");
+		}
+	}
 	closeDB($DB);
+
+	$DB2 = openDB();
+	$result2 = mysqli_query($DB2, "CALL delete_account(".$_SESSION["accountid"].")");
+	if(!$result2) {
+		echo("Failed to delete the account from database:<br>");
+		echo mysqli_error($DB2);
+	}
+	closeDB($DB2);
 	
-	header("Location: ../php/logout.php");
+	//header("Location: ../php/logout.php");
 	
 	die();
 ?>
